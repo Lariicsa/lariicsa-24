22941688923d206cc57bcb47c9e11f5c96c2c505
Larissa <dejamepienso@gmail.com>
2024-08-29 12:31:36 -0600

    Set translation in Home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,65 +1,63 @@
 <!-- @format -->
 <script setup>
-	import { useRouter } from "vue-router";
-	import { ref } from "vue";
-	import AppButton from "@/components/AppButton.vue";
-	import AppContact from "@/components/AppContact.vue";
-	import AppToaster from "@/components/AppToaster.vue";
-	import CircleImage from "@/components/CircleImage.vue";
+import { ref, computed } from "vue";
+import { useRouter } from "vue-router";
+import { useI18n } from "vue-i18n";
+import Tr from "@/i18n/translation";
+import AppButton from "@/components/AppButton.vue";
+import AppContact from "@/components/AppContact.vue";
+import AppToaster from "@/components/AppToaster.vue";
+import CircleImage from "@/components/CircleImage.vue";
 
-	const router = useRouter();
 
-	const showMessage = ref(false);
-	const copyEmailToClipboard = () => {
-		navigator.clipboard.writeText("lariicsa@gmail.com");
-		showMessage.value = true;
-		setTimeout(() => {
-			showMessage.value = false;
-		}, 3000);
-	};
+const { t, locale } = useI18n();
+const supportedLocales = Tr.supportedLocales;
 
-	const goTo = (path) => {
-		router.push({ path: `/${path}` });
-	};
+const router = useRouter();
+
+const showMessage = ref(false);
+const copyEmailToClipboard = () => {
+	navigator.clipboard.writeText("lariicsa@gmail.com");
+	showMessage.value = true;
+	setTimeout(() => {
+		showMessage.value = false;
+	}, 3000);
+};
+
+const homeTranslation = computed(() => {
+	return t("nav.menu.0.name")
+});
+
+
+
+const goTo = (path) => {
+	router.push({ path: `/${path}` });
+};
 </script>
 
 <template>
 	<div class="w-full flex-col max-w-[1300px] flex justify-center mx-auto">
 		<main class="flex w-full flex-col md:flex-row items-center justify-between">
-			<div
-				class="flex flex-col w-full md:w-4/6 mt-[32px] sm:mt-[64px] mx-auto items-center sm:items-start">
-				<div
-					class="text-[40px] sm:text-[64px] text-center sm:text-left font-extrabold text-[#e0e0e0]">
-					Hi,
-					<h1
-						class="inline-block bg-clip-text text-transparent bg-gradient-to-r from-deg-peach to-deg-purple">
-						I'm Larissa Avila
+			<div class="flex flex-col w-full md:w-4/6 mt-[32px] sm:mt-[64px] mx-auto items-center sm:items-start">
+				<div class="text-[40px] sm:text-[64px] text-center sm:text-left font-extrabold text-[#e0e0e0]">
+					{{ $t("home.hi") }}, {{ $t("home.im") }}
+					<h1 class="inline-block bg-clip-text text-transparent bg-gradient-to-r from-deg-peach to-deg-purple">
+						Larissa Avila
 					</h1>
 				</div>
 				<p
 					class="flex items-center text-center sm:text-left sm:items-start w-auto text-[#e0e0e0] font-semibold text-[28px] sm:text-[44px]">
-					Frontend / UI Developer and HTML/CSS ninja.
+					{{ $t("home.developer") }} {{ $t("home.ninja") }}.
 				</p>
 
-				<p
-					class="w-full text-[#9FA1A4] mt-[24px] text-[17px] sm:text-[20px] font-light text-center sm:text-left">
-					Frontend experience for over 6 years (included ux/ui design). <br />
-					Looking for new challenges where I can apply and improve my skills in
-					every different project.
+				<p class="w-full text-[#9FA1A4] mt-[24px] text-[17px] sm:text-[20px] font-light text-center sm:text-left">
+					{{ $t("home.abstract") }}
 				</p>
 
-				<div
-					class="flex w-full justify-center sm:justify-start my-[36px] sm:my-[64px]">
-					<AppButton
-						@click="goTo('experience')"
-						color="peach"
-						size="lg"
-						class="mr-[24px] sm:mr-[48px]"
-						>Experience</AppButton
-					>
-					<AppButton @click="goTo('about')" color="pink" size="lg"
-						>About me</AppButton
-					>
+				<div class="flex w-full justify-center sm:justify-start my-[36px] sm:my-[64px]">
+					<AppButton @click="goTo('experience')" color="peach" size="lg" class="mr-[24px] sm:mr-[48px]">{{ $t("home.btnExperience") }}
+					</AppButton>
+					<AppButton @click="goTo('about')" color="pink" size="lg">{{  $t("home.btnAbout")  }}</AppButton>
 				</div>
 
 				<AppContact @clickOnEnvelope="copyEmailToClipboard" />
