f1c6ca0263dfd97502bbf0ec120e68a9c057913e
Larissa <dejamepienso@gmail.com>
2024-08-29 14:28:44 -0600

    Set translation in About

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,16 +1,22 @@
 <!-- @format -->
+<script setup>
+import { useI18n } from "vue-i18n";
+import Tr from "@/i18n/translation";
 
+const { t, locale } = useI18n();
+const supportedLocales = Tr.supportedLocales;
+</script>
 <template>
 	<div class="flex flex-col items-center w-full mb-[120px] md:mb-0">
 		<h1 class="mt-[56px] text-[44px] text-[#E5E5E5] text-center font-semibold">
-			About Me
+			{{ $t("about.title") }}
 		</h1>
 
 		<div
-			class="flex items-center md:items-stretch bg-[#FFE194] flex-col sm:flex-row w-full max-w-[1124px] h-auto mt-[64px] md:mt-[108px] rounded-bl-[122px] rounded-tr-[122px] relative">
+			class="cont1 flex items-center md:items-stretch bg-[#FFE194] flex-col sm:flex-row w-full max-w-[1124px] h-auto mt-[64px] md:mt-[108px] rounded-br-[80px]  md:rounded-bl-[122px] rounded-tl-[80px] md:rounded-tr-[122px] relative">
 			<p
 				class="big-txt text-[70px] sm:text-[120px] absolute top-[-72px] sm:top-[-120px] left-auto text-center w-full z-0 opacity-5 font-bold text-[#FFE194]">
-				About Me
+				{{ $t("about.title") }}
 			</p>
 
 			<div
@@ -21,22 +27,14 @@
 			</div>
 
 			<div
-				class="flex flex-col w-xl sm:w-2/3 bg-transparent md:bg-[#FFE194] text-[#373737] rounded-tr-[122px] px-[24px] md:px-[48px] py-[48px] relative z-[2]">
-				<h2
-					class="text-center md:text-left font-semibold md:text-[32px] text-[24px]">
-					Larissa Avila
+				class="flex flex-col w-xl sm:w-2/3 bg-transparent md:bg-[#FFE194] text-[#373737] rounded-tl-[80px] md:rounded-tr-[122px] px-[24px] md:px-[48px] py-[48px] relative z-[2]">
+				<h2 class="text-center md:text-left font-semibold md:text-[32px] text-[24px]">
+					{{ $t("about.fullName") }}
 				</h2>
-
-				<p
-					class="text-center md:text-left text-[14px] md:text-[20px] mt-[24px] md:mt-[64px]">
-					I'm a Frontend Developer with UI Design background and strong skills
-					implementing HTML & CSS in different environments like Vue, React,
-					Angular, etc. 
-					<br/>	<br/>
-					I have always enjoyed laying out and all related with
-					user interactions. I think one of the skills that makes me different,
-					in addition to my wide CSS knowledge, is to know all about user
-					interfaces, design patterns and development interactions.
+				<p class="text-center md:text-left text-[14px] md:text-[20px] mt-[24px] md:mt-[32px]">
+					{{ $t("about.abstract.0") }}
+					<br /> <br />
+					{{ $t("about.abstract.1") }}
 				</p>
 			</div>
 		</div>
