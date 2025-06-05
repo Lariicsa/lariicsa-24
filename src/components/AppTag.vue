4c268ec373dcfe0f4610f7dcd6f2b944e79ab066
Larissa <dejamepienso@gmail.com>
2025-06-05 13:28:39 -0600

    Update job cards

--- src/components/AppTag.vue
+++ src/components/AppTag.vue
@@ -2,7 +2,7 @@
 <script setup></script>
 <template>
 	<div
-		class="wx-auto mr-[12px] mt-[8px] sm:my-[6px] flex justify-center items-center bg-[#d6e7fa] text-[#2d2e3b] text-[10px] sm:text-[12px] rounded-full py-[4px] px-[8px] sm:px-[12px]">
+		class="wx-auto mr-[12px] mt-[8px] sm:my-[4px] flex justify-center items-center bg-[#d6e7fa] text-[#2d2e3b] text-[10px] sm:text-[12px] rounded-full py-[4px] px-[8px] sm:px-[12px]">
 		<slot></slot>
 	</div>
 </template>
