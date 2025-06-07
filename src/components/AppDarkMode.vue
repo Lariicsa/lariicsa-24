192f1f34ab5d0f574f37d4e96108e5b8f509447f
Larissa <dejamepienso@gmail.com>
2025-06-06 18:02:44 -0600

    Add toggle in darkmode logo

--- src/components/AppDarkMode.vue
+++ src/components/AppDarkMode.vue
@@ -1,15 +1,27 @@
 <template>
-  <div aria-label="dark mode" @click="setDarkMode()">
+  <div aria-label="dark mode" @click="setDarkMode()" class="md:hover:cursor-pointer text-white">
     <font-awesome-icon icon="fas fa-moon" class="w-full h-auto" />
   </div>
 </template>
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 
 const isDarkMode = ref(false)
+const space = ref(document.querySelector('#app'))
 
-const setDarkMode = ()=>{
+onMounted(()=>{
+  space.value
+})
+
+const setDarkMode = () => {
   isDarkMode.value = !isDarkMode.value
-  localStorage.getItem()
+  const darkMode = localStorage.getItem('dm')
+  if (darkMode === null || darkMode === 'nain') {
+    localStorage.setItem('dm', 'yei')
+    space.value.classList.add('dark')
+  } else {
+    localStorage.setItem('dm', 'nain')
+    space.value.classList.remove('dark')
+  }
 }
 </script>
