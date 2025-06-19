bf0225cae575df45e36df143a848c2fcb281cc96
Larissa <dejamepienso@gmail.com>
2025-06-19 16:39:10 -0600

    Update about ui

--- src/components/AppButton.vue
+++ src/components/AppButton.vue
@@ -46,21 +46,21 @@ const props = defineProps({
 </script>
 <template>
 	<a v-if="isLink" :href="link" target="_blank"
-		class="flex flex-col justify-center items-center border-none rounded-full active:saturate-200 sm:hover:saturate-150 text-center"
+		class="flex flex-col justify-center items-center border-none rounded-lg active:saturate-200 sm:hover:saturate-150 text-center"
 		role="button"
 		:class="`${color} ${size}`">
 		<slot></slot>
 	</a>
 
 	<RouterLink v-else-if="isRoute" :to="to"
-		class="flex flex-col justify-center items-center border-none rounded-full active:saturate-200 sm:hover:saturate-150 text-center"
+		class="flex flex-col justify-center items-center border-none rounded-lg active:saturate-200 sm:hover:saturate-150 text-center"
 		role="button"
 		:class="`${color} ${size}`">
 		<slot></slot>
 	</RouterLink>
 
 	<button v-else role="button"
-		class="flex flex-col justify-center items-center border-none rounded-full active:saturate-200 sm:hover:saturate-150"
+		class="flex flex-col justify-center items-center border-none rounded-lg active:saturate-200 sm:hover:saturate-150"
 		:class="`${color} ${size}`">
 		<slot></slot>
 	</button>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,6 +4,7 @@ import Tr from "@/i18n/translation";
 
 const { t, locale } = useI18n();
 const supportedLocales = Tr.supportedLocales;
+const picAbout = '../about_me.jpg'
 </script>
 <template>
 	<div class="flex flex-col items-center w-full mb-[120px] md:mb-0 h-full min-h-[calc(100vh-116px)]">
@@ -12,21 +13,21 @@ const supportedLocales = Tr.supportedLocales;
 		</h1>
 
 		<div
-			class="cont1 flex items-center md:items-stretch bg-[#FFE194] dark:bg-black flex-col sm:flex-row w-full max-w-[1124px] h-auto mt-[64px] md:mt-[108px] rounded-br-[80px]  md:rounded-bl-[122px] rounded-tl-[80px] md:rounded-tr-[122px] relative">
+			class="cont1 flex items-center md:items-stretch bg-[#FFE194] dark:bg-black flex-col sm:flex-row w-full max-w-[1124px] h-auto mt-[64px] md:mt-[108px] rounded-br-2xl  md:rounded-bl-[66px] rounded-tl-2xl md:rounded-tr-[122px] relative">
 			<p
 				class="big-txt text-[70px] sm:text-[120px] absolute top-[-72px] sm:top-[-120px] left-auto text-center w-full z-0 opacity-5 font-bold text-[#FFE194] dark:text-gray-100">
 				{{ $t("about.title") }}
 			</p>
 
 			<div
-				class="flex w-auto h-[200px] md:h-auto sm:w-1/3 bg-transparent md:bg-[#FFE194] rounded-bl-[122px] relative z-[2]">
+				class="flex w-auto h-[200px] md:h-auto sm:w-1/3 bg-transparent md:bg-[#FFE194] rounded-bl-[33px] relative z-[2]">
 				<div
-					class="rounded-full md:rounded-bl-[122px] md:rounded-br-none md:rounded-tr-none md:rounded-tl-none overflow-hidden w-[200px] h-[200px] sm:w-[396px] sm:h-auto bg-cover bg-no-repeat bg-right mt-[24px] md:mt-0"
-					style="background-image: url(../about_me.jpg)"></div>
+					class="rounded-xl md:rounded-bl-3xl md:rounded-br-none md:rounded-tr-none md:rounded-tl-none overflow-hidden w-[200px] h-[200px] sm:w-[396px] sm:h-auto bg-cover bg-no-repeat bg-right mt-[24px] md:mt-0"
+					:style="`background-image: url(${picAbout})`"></div>
 			</div>
 
 			<div
-				class="flex flex-col  w-xl sm:w-2/3 bg-transparent md:bg-[#FFE194] dark:bg-black text-[#373737] dark:text-gray-300 rounded-tl-[80px] md:rounded-tr-[122px] px-[24px] md:px-[48px] py-[48px] relative z-[2]">
+				class="flex flex-col  w-xl sm:w-2/3 bg-transparent md:bg-[#FFE194] dark:bg-black text-[#373737] dark:text-gray-300 rounded-tl-[40px] md:rounded-tr-3xl px-[24px] md:px-[48px] py-[48px] relative z-[2]">
 				<h2 class="text-center md:text-left font-semibold md:text-[32px] text-[24px]">
 					{{ $t("about.fullName") }}
 				</h2>
