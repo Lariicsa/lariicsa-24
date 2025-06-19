c99501077618867bc8d1c9b9d125e63bb24b1d24
Larissa <dejamepienso@gmail.com>
2025-06-19 12:19:38 -0600

    Update links

--- src/router/index.js
+++ src/router/index.js
@@ -34,7 +34,7 @@ const router = createRouter({
 					component: () => import("@/views/ExperienceView.vue"),
 				},
 				{
-					path: "pdfviewer",
+					path: "resume",
 					name: "pdfviewer",
 
 					component: () => import("@/views/AppPDFViewer.vue"),
